89f0267dbb9ab3f6a5d90df5b9bfc17ccea80a03
chaman bharti <chaman.bharti84@gmail.com>
2018-10-25 12:48:09 +0530

    Proxy Design Pattern Class

---
+++ pro-spring5-aop/src/main/java/com/aop/ProSpring5AopApplication.java
@@ -0,0 +1,12 @@
+package com.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSpring5AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSpring5AopApplication.class, args);
+	}
+}
